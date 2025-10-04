ac74a5705398466326c805fa35c63883054b851a
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-10-04 16:11:29 -0600

    Se crea el método InsertarEmpleado en la clase Empleado del BusinessLogic

---
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI.BusinessLogic/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PruebaTecnicaAPI.BusinessLogic.Interface;
+using PruebaTecnicaAPI.DataAccess.Interface;
+
+namespace PruebaTecnicaAPI.BusinessLogic
+{
+    public class Empleado : Interface.IEmpleado
+    {
+        private readonly DataAccess.Interface.IEmpleado empleado;
+
+
+        public Empleado(DataAccess.Interface.IEmpleado _empleado)
+        {
+           this.empleado = _empleado;
+        }
+
+        public void InsertarEmpleado(Model.Empleado emp)
+        {
+            empleado.InsertarEmpleado(emp);
+        }
+    }
+}
--- PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI/Controllers/EmpleadoController.cs
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI/Controllers/EmpleadoController.cs
@@ -11,12 +11,37 @@ namespace PruebaTecnicaAPI.Controllers
     {
         IEmpleado empleado;
         IEnumerable<Model.Empleado> listaEmpleados = Enumerable.Empty<Model.Empleado>();
-        private readonly ConnectionManagerDbContext _context;
+        private readonly ConnectionManagerDbContext context;
 
-        public EmpleadoController(IEmpleado empleado, ConnectionManagerDbContext context)
+        public EmpleadoController(IEmpleado _empleado, ConnectionManagerDbContext _context)
         {
-            this.empleado = empleado;
-            this._context = context;
+            this.empleado = _empleado;
+            this.context = _context;
+        }
+
+        [HttpPost]
+        [Route("CrearEmpleado")]
+
+        public async Task<ActionResult> CrearEmpleado([FromBody] Model.Empleado emp)
+        {
+            try
+            {
+                empleado.InsertarEmpleado(emp);
+
+                return Ok(new
+                {
+                    success = true,
+                    estado = StatusCode(200)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    estado = ex.Message.ToString()
+                });
+            }
         }
     }
 }
