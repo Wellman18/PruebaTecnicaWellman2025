ad0f904e07982ca3a5534b8a370c2f29ef3844a3
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-10-04 15:32:36 -0600

    Se crean los modelos de TipoIdentificacion y Empleado

---
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI.Model/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaAPI.Model
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public string NombrePuesto { get; set; }
+
+        public int IdTipoIdentificacion { get; set; }
+
+        public string NumeroIdentificacion { get; set; }
+    }
+}
---
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaTecnicaAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+
+    public class EmpleadoController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
