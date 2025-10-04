6522ca05bf291fcfc8f4bc2f9ac2b38a7d2ecc87
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-10-04 15:50:10 -0600

    Se crea el proyecto DataAccess.Interface

---
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI.DataAccess.Interface/IConnectionManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaAPI.Model;
+
+namespace PruebaTecnicaAPI.DataAccess.Interface
+{
+    public interface IConnectionManagerDbContext
+    {
+        DbSet<Empleado> Empleado { get; set; }
+
+        DbSet<TipoIdentificacion> TipoIdentificacion { get;set; }
+    }
+}
--- PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI.DataAccess/ConnectionManagerDbContext.cs
+++ PruebaTecnicaAPI/PruebaTecnicaAPI/PruebaTecnicaAPI.DataAccess/ConnectionManagerDbContext.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PruebaTecnicaAPI.Model;
 
 namespace PruebaTecnicaAPI.DataAccess
 {
-    internal class ConnectionManagerDbContext
+    public class ConnectionManagerDbContext : DbContext
     {
+        public ConnectionManagerDbContext(DbContextOptions<ConnectionManagerDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+
+        public DbSet<Empleado> Empleado { get; set; }
+
+        public DbSet<TipoIdentificacion> TipoIdentificacion { get; set; }
     }
 }
